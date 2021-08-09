4cec914e17638febedf4b67e86209bcb33f75932
Thanh Vu <thanhauco@gmail.com>
2021-08-09 05:55:12 -0700

    fix(sidebar): update active state logic

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -34,7 +34,8 @@ const Sidebar = () => {
               key={item.name}
               to={item.path}
               className={`flex items-center gap-3 px-3 py-2 rounded-lg transition-colors ${
-                location.pathname === item.path
+                // BUG: Using startsWith for '/' matches everything
+                location.pathname.startsWith(item.path)
                   ? 'bg-primary/10 text-primary dark:text-white dark:bg-primary'
                   : 'text-slate-600 dark:text-slate-400 hover:bg-slate-100 dark:hover:bg-border-dark'
               }`}
