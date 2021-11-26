24371299c40d6c22e63208453a25d68c80743eef
Thanh Vu <thanhauco@gmail.com>
2021-11-25 21:12:49 -0800

    feat: add technical information and tabs to transaction details

--- src/pages/Transactions.tsx
+++ src/pages/Transactions.tsx
@@ -143,9 +143,153 @@ const Transactions = () => {
             </div>
           </div>
         </div>
+        {/* Details Tabs & Tables */}
+        <div className="bg-white dark:bg-surface-dark border border-slate-200 dark:border-border-dark rounded-xl overflow-hidden">
+          {/* Tabs */}
+          <div className="border-b border-slate-200 dark:border-border-dark">
+            <nav aria-label="Tabs" className="flex overflow-x-auto">
+              <button className="px-6 py-4 text-sm font-bold text-primary border-b-2 border-primary whitespace-nowrap bg-primary/5">
+                Overview
+              </button>
+              <button className="px-6 py-4 text-sm font-medium text-slate-500 dark:text-slate-400 hover:text-slate-900 dark:hover:text-white border-b-2 border-transparent hover:border-border-dark whitespace-nowrap transition-colors">
+                Internal Txns
+                <span className="ml-2 py-0.5 px-2 rounded-full bg-slate-100 dark:bg-border-dark text-xs text-slate-600 dark:text-white">3</span>
+              </button>
+              <button className="px-6 py-4 text-sm font-medium text-slate-500 dark:text-slate-400 hover:text-slate-900 dark:hover:text-white border-b-2 border-transparent hover:border-border-dark whitespace-nowrap transition-colors">
+                Logs
+              </button>
+              <button className="px-6 py-4 text-sm font-medium text-slate-500 dark:text-slate-400 hover:text-slate-900 dark:hover:text-white border-b-2 border-transparent hover:border-border-dark whitespace-nowrap transition-colors">
+                State Changes
+              </button>
+            </nav>
+          </div>
+          {/* Content Area */}
+          <div className="p-6">
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-12 gap-y-8">
+              {/* Left Column: General Info */}
+              <div className="flex flex-col gap-0">
+                <h3 className="text-lg font-bold mb-4 flex items-center gap-2 text-slate-900 dark:text-white">
+                  <span className="material-symbols-outlined text-primary">info</span>
+                  General Information
+                </h3>
+                <div className="py-4 border-b border-slate-100 dark:border-border-dark grid grid-cols-[140px_1fr] items-center gap-4">
+                  <div className="flex items-center gap-1 text-slate-500 dark:text-slate-400 text-sm">
+                    <span className="material-symbols-outlined text-[16px]">help</span>
+                    Transaction Hash:
+                  </div>
+                  <div className="text-sm break-all font-mono text-slate-900 dark:text-white">0x3f5ce5fb...9e72</div>
+                </div>
+                <div className="py-4 border-b border-slate-100 dark:border-border-dark grid grid-cols-[140px_1fr] items-center gap-4">
+                  <div className="flex items-center gap-1 text-slate-500 dark:text-slate-400 text-sm">
+                    <span className="material-symbols-outlined text-[16px]">help</span>
+                    Status:
+                  </div>
+                  <div>
+                    <span className="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-green-500/10 text-green-500 border border-green-500/20">
+                      Success
+                    </span>
+                  </div>
+                </div>
+                <div className="py-4 border-b border-slate-100 dark:border-border-dark grid grid-cols-[140px_1fr] items-center gap-4">
+                  <div className="flex items-center gap-1 text-slate-500 dark:text-slate-400 text-sm">
+                    <span className="material-symbols-outlined text-[16px]">help</span>
+                    Block:
+                  </div>
+                  <div className="flex items-center gap-2 text-sm">
+                    <a className="text-primary hover:underline" href="#">18483921</a>
+                    <span className="text-xs text-slate-500 dark:text-slate-400 px-1.5 py-0.5 bg-slate-100 dark:bg-border-dark rounded">12 Confirmations</span>
+                  </div>
+                </div>
+                <div className="py-4 border-b border-slate-100 dark:border-border-dark grid grid-cols-[140px_1fr] items-center gap-4">
+                  <div className="flex items-center gap-1 text-slate-500 dark:text-slate-400 text-sm">
+                    <span className="material-symbols-outlined text-[16px]">help</span>
+                    Timestamp:
+                  </div>
+                  <div className="text-sm flex flex-col sm:flex-row sm:items-center gap-1 text-slate-900 dark:text-white">
+                    <span className="material-symbols-outlined text-[16px] text-slate-500 dark:text-slate-400">schedule</span>
+                    <span>12 secs ago</span>
+                    <span className="text-slate-500 dark:text-slate-400">(Oct-27-2023 10:45:12 AM +UTC)</span>
+                  </div>
+                </div>
+                <div className="py-4 border-b border-slate-100 dark:border-border-dark grid grid-cols-[140px_1fr] items-center gap-4">
+                  <div className="flex items-center gap-1 text-slate-500 dark:text-slate-400 text-sm">
+                    <span className="material-symbols-outlined text-[16px]">help</span>
+                    Value:
+                  </div>
+                  <div className="text-sm bg-slate-100 dark:bg-border-dark/30 px-3 py-2 rounded-lg inline-block w-fit text-slate-900 dark:text-white">
+                    <span className="font-bold">2.5 ETH</span>
+                    <span className="text-slate-500 dark:text-slate-400 ml-1">($4,200.50)</span>
+                  </div>
+                </div>
+              </div>
+              {/* Right Column: Technical Details */}
+              <div className="flex flex-col gap-0">
+                <h3 className="text-lg font-bold mb-4 flex items-center gap-2 text-slate-900 dark:text-white">
+                  <span className="material-symbols-outlined text-primary">settings_ethernet</span>
+                  Technical Details
+                </h3>
+                <div className="py-4 border-b border-slate-100 dark:border-border-dark grid grid-cols-[140px_1fr] items-center gap-4">
+                  <div className="flex items-center gap-1 text-slate-500 dark:text-slate-400 text-sm">
+                    <span className="material-symbols-outlined text-[16px]">help</span>
+                    Gas Limit:
+                  </div>
+                  <div className="text-sm font-mono text-slate-900 dark:text-white">21,000</div>
+                </div>
+                <div className="py-4 border-b border-slate-100 dark:border-border-dark grid grid-cols-[140px_1fr] items-center gap-4">
+                  <div className="flex items-center gap-1 text-slate-500 dark:text-slate-400 text-sm">
+                    <span className="material-symbols-outlined text-[16px]">help</span>
+                    Gas Used by Txn:
+                  </div>
+                  <div className="text-sm font-mono text-slate-900 dark:text-white">
+                    21,000 <span className="text-slate-500 dark:text-slate-400 text-xs">(100%)</span>
+                  </div>
+                </div>
+                <div className="py-4 border-b border-slate-100 dark:border-border-dark grid grid-cols-[140px_1fr] items-center gap-4">
+                  <div className="flex items-center gap-1 text-slate-500 dark:text-slate-400 text-sm">
+                    <span className="material-symbols-outlined text-[16px]">help</span>
+                    Gas Price:
+                  </div>
+                  <div className="text-sm font-mono text-slate-900 dark:text-white">
+                    0.000000025 ETH <span className="text-slate-500 dark:text-slate-400 text-xs">(25 Gwei)</span>
+                  </div>
+                </div>
+                <div className="py-4 border-b border-slate-100 dark:border-border-dark grid grid-cols-[140px_1fr] items-center gap-4">
+                  <div className="flex items-center gap-1 text-slate-500 dark:text-slate-400 text-sm">
+                    <span className="material-symbols-outlined text-[16px]">help</span>
+                    Nonce:
+                  </div>
+                  <div className="text-sm font-mono text-slate-500 dark:text-slate-400">
+                    <span className="text-slate-900 dark:text-white">142</span> (Position in block: 12)
+                  </div>
+                </div>
+                <div className="py-4 border-b border-slate-100 dark:border-border-dark grid grid-cols-[140px_1fr] items-start gap-4">
+                  <div className="flex items-center gap-1 text-slate-500 dark:text-slate-400 text-sm pt-1">
+                    <span className="material-symbols-outlined text-[16px]">help</span>
+                    Input Data:
+                  </div>
+                  <div className="w-full">
+                    <textarea 
+                      className="w-full h-24 bg-slate-50 dark:bg-background-dark border border-slate-200 dark:border-border-dark rounded p-3 text-xs font-mono text-slate-500 dark:text-slate-400 resize-none focus:outline-none focus:border-primary" 
+                      readOnly 
+                      value="0x608060405234801561001057600080fd5b5061012f806100206000396000f30060806040526004361061004c576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff16806306661abd146100515780634f2be968146100785780636c236a291461008d575b600080fd5b34801561005c57600080fd5b506100656100b2565b6040518082815260200191505060405180910390f35b34801561008357600080fd5b5061008b6100b8565b005b34801561009857600080fd5b506100a16100ca565b6040518082815260200191505060405180910390f35b60005481565b600055565b600054905600a165627a7a723058204618a8f276239126d403932782970720516757648356149491632506669866210029"
+                    />
+                    <div className="flex justify-end mt-1">
+                      <button className="text-primary text-xs font-bold hover:underline">Decode Input Data</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Footer of card */}
+          <div className="bg-slate-50 dark:bg-background-dark/50 p-4 border-t border-slate-200 dark:border-border-dark text-center text-xs text-slate-500 dark:text-slate-400">
+            <p>Transaction included in block <span className="text-primary">#18483921</span> at position 12</p>
+          </div>
+        </div>
       </div>
     </Layout>
   );
 };
 
 export default Transactions;
+
